3db990ab78edaa851fd48c40f07f9a181611f320
geisaandrade <59211224+geisaandrade@users.noreply.github.com>
2020-03-01 19:06:47 -0300

    Criando a classe tabuleiro
    -scripts de posição;

---
+++ MyXadrex/MyXadrex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+
+namespace MyXadrex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posiçao: " + P);
+            Console.ReadLine();
+        }
+    }
+}
